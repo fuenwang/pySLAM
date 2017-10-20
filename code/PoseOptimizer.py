2a2cbde749cf7718d286fb9f1335047d9e8489ab
Fu-En.Wang <fulton84717@gmail.com>
2017-10-20 17:46:31 +0800

    gg

--- code/PoseOptimizer.py
+++ code/PoseOptimizer.py
@@ -7,19 +7,37 @@ from scipy.optimize import least_squares
 import Dataset
 import Cameras
 
+#CameraModel = Cameras.CameraModel()
+
+def PhotometricError(pose, points, img1, img2, mask, obj, h, w):
+    points_2D = obj.PerspectiveProject(points, pose)
+    
+    img1_flat = img1.reshape([h * w])
+    #img2_flat = img2.reshape([h * w])
+    return np.abs(img1_flat[mask] - (img2[points_2D[:, 1], points_2D[:, 0]].reshape([h * w]))[mask])
+
 class PoseOptimizer:
-    def __init__(self):
-        pass
+    def __init__(self, height, width):
+        self.h = height
+        self.w = width
+        self.camera = Cameras.CameraModel(height, width)
     
     def TwoViewPose(self, img1, depth1, img2):
     #   Calculate two-view pose
-        depth_mask1 = (depth1 != -1)
-        depth_mask2 = (depth2 != -1)
+        h = self.h
+        w = self.w
+        model = self.camera
+        #img1_flat = img1.reshape([h * w])
+        #img2_flat = img2.reshape([h * w])
+        depth_mask = (depth1 != -1).reshape([h*w])
 
+        points_3D = model.PointFromDepth(depth)
 
+        
 if __name__ == '__main__':
     data = Dataset.Dataset('../data')
 
     [img1, depth1] = data[2]
     [img2, depth2] = data[5]
-
+    print img2.shape
+    print depth2.shape
